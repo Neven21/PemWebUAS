ea1ae0cbd17e05a1aed18a64af60d640e1f0db3f
Neven21 <gregoriusneven@yahoo.com>
2020-05-05 11:59:07 +0700

    Test Commit

--- Web/resources/views/welcome.blade.php
+++ Web/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Authentic Restaurant
                 </div>
 
                 <div class="links">
